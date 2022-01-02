716a1bb45290aacef5baab7b20f63734830bec3f
Antony.C <93124252+CodingBDX@users.noreply.github.com>
2022-01-02 09:38:32 +0100

    Add files via upload

---
+++ prototype/index.js
@@ -0,0 +1,44 @@
+
+const PORT = process.env.PORT || 5000;
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+
+const url = 'https://www.programme-tv.net/programme/canal-5'
+
+app.get('/', function (req, res) {
+    res.json('This is my webscraper')
+})
+
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
+            
+
+
+            $('.mainBroadcastCard-title', html).each(function () { //<-- cannot be a function expression
+                const title = $(this).text()
+                const url = $(this).find('a').attr('href')
+                articles.push({
+                    title,
+                    url
+                });
+               
+            })
+          
+            
+            res.json(articles)
+        }).catch(err => console.log(err))
+
+})
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
